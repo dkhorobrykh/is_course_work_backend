e329e7d1a742173892832fd11b47e5e204159b76
KseniyaNesterenko <115107951+KseniyaNesterenko@users.noreply.github.com>
2024-12-12 01:48:09 +0300

    🦅Добавление учета характеристик пассажира

--- src/main/java/ru/itmo/is/course_work/controller/ShipController.java
+++ src/main/java/ru/itmo/is/course_work/controller/ShipController.java
@@ -6,8 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.itmo.is.course_work.model.Ship;
+import ru.itmo.is.course_work.model.UserData;
 import ru.itmo.is.course_work.model.dto.ShipDto;
 import ru.itmo.is.course_work.service.ShipService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/ships")
@@ -25,5 +29,11 @@ public class ShipController {
     public ResponseEntity<ShipDto> addNewShip(@RequestBody @Validated ShipDto shipDto) {
         ShipDto createdShip = shipService.addNewShipWithStatus(shipDto);
         return new ResponseEntity<>(createdShip, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/find")
+    public ResponseEntity<List<Ship>> findShipsForPassenger(@RequestBody UserData userData) {
+        List<Ship> ships = shipService.findShipsForPassenger(userData);
+        return new ResponseEntity<>(ships, HttpStatus.OK);
     }
 }
--- src/main/java/ru/itmo/is/course_work/repository/ShipRepository.java
+++ src/main/java/ru/itmo/is/course_work/repository/ShipRepository.java
@@ -3,5 +3,8 @@ package ru.itmo.is.course_work.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.itmo.is.course_work.model.Ship;
 
+import java.util.List;
+
 public interface ShipRepository extends JpaRepository<Ship, Long> {
+    List<Ship> findByServiceClasses_NameIn(List<String> serviceClassNames);
 }
