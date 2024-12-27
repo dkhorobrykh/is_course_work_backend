54fc5e9b36db60a9a6dd069fd4cbe54f363c29f7
DKhorobrykh <horobryh.daniil@yandex.ru>
2024-12-27 14:48:20 +0500

    fix assign flight

--- src/main/java/ru/itmo/is/course_work/repository/FlightScheduleRepository.java
+++ src/main/java/ru/itmo/is/course_work/repository/FlightScheduleRepository.java
@@ -1,7 +1,21 @@
 package ru.itmo.is.course_work.repository;
 
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.itmo.is.course_work.model.FlightSchedule;
 
 public interface FlightScheduleRepository extends JpaRepository<FlightSchedule, Long> {
+    @Query("""
+        SELECT fs
+        FROM FlightSchedule fs
+        WHERE fs.id = :id""")
+    Optional<FlightSchedule> findByIdEquals(Long id);
+
+    @EntityGraph(type = EntityGraphType.LOAD, attributePaths = {"flight", "planetDeparture", "planetArrival", "flight" +
+        ".ship"})
+    List<FlightSchedule> findAll();
 }
